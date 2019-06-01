6abc9a0d1ecf811c49be91a08815cfc013995eb1
Dhanya Premkumar <dpremkumar@pac.bluecross.ca>
2019-05-31 18:17:35 -0700

    Initial Commit

---
+++ IntegerAdd/Controllers/AddController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using IntegerAdd.Models;
+
+namespace IntegerAdd.Controllers
+{
+    public class AddController: ApiController
+    {
+
+        [Route("add/input1/{input1}/input2/{input2}")]
+        [HttpPost]
+        public HttpResponseMessage AddInput(int input1, int input2)
+        {
+            int result = input1 + input2;
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+
+        [Route("add/integers")]
+        [HttpPost]
+        public HttpResponseMessage PostData(DataInput input)
+        {
+            int result = input.Input1 + input.Input2;
+
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+
+    }
+}
---
+++ IntegerAdd/Models/DataInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntegerAdd.Models
+{
+    public class DataInput
+    {
+        public int Input1 { get; set; }
+
+        public int Input2 { get; set; }
+    }
+}
